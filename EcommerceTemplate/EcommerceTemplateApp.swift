8be5bb63a674f509ffd087b5a4273371859d64c8
guicompeng <guicompeng@gmail.com>
2022-02-16 18:49:27 -0300

    start project

---
+++ EcommerceTemplate/EcommerceTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceTemplateApp.swift
+//  EcommerceTemplate
+//
+//  Created by Guilherme Assis on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
