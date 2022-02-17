e085900f16afab683c36a50a1207eb86ab208b70
guicompeng <guicompeng@gmail.com>
2022-02-16 23:30:32 -0300

    Created login and splash screens

---
+++ EcommerceTemplate/Constants/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  EcommerceTemplate
+//
+//  Created by Guilherme Assis on 16/02/22.
+//
+
+import Foundation
---
+++ EcommerceTemplate/Constants/Fonts.swift
@@ -0,0 +1,8 @@
+//
+//  Fonts.swift
+//  EcommerceTemplate
+//
+//  Created by Guilherme Assis on 16/02/22.
+//
+
+import Foundation
