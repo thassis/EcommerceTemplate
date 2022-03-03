822f4203903211d2d634efcf1362bf91bd913c51
Thiago Assis <thiagoassis.99@gmail.com>
2022-03-03 11:25:54 -0300

    fix purple warning

--- EcommerceTemplate/ViewModels/ProductsViewModel.swift
+++ EcommerceTemplate/ViewModels/ProductsViewModel.swift
@@ -13,8 +13,6 @@ class ProductsViewModel: ObservableObject {
     @Published private(set) var products = Products()
     
     func getNewProducts() {
-        
-        
         let url = URL(string: "https://apisconstants.herokuapp.com/")!
         
         var request = URLRequest(url: url)
@@ -23,13 +21,16 @@ class ProductsViewModel: ObservableObject {
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
-                let responseJson = try! JSON(data: data)
-                let arrayCount = responseJson.array?.count ?? 0
-                for i in 0..<arrayCount {
-                    let product = responseJson[i]
-                    let productInfo = ProductInfo(name: product["name"].string ?? "", price: product["price"].float ?? 0, discountPrice: product["discountPrice"].float ?? 0, description: product["description"].string ?? "", imageURL: product["image"].string ?? "")
-                    print(productInfo)
-                    self.products.addProduct(productInfo)
+                DispatchQueue.main.async { [weak self] in
+                    
+                    let responseJson = try! JSON(data: data)
+                    let arrayCount = responseJson.array?.count ?? 0
+                    for i in 0..<arrayCount {
+                        let product = responseJson[i]
+                        let productInfo = ProductInfo(name: product["name"].string ?? "", price: product["price"].float ?? 0, discountPrice: product["discountPrice"].float ?? 0, description: product["description"].string ?? "", imageURL: product["image"].string ?? "")
+                        print(productInfo)
+                        self!.products.addProduct(productInfo)
+                    }
                 }
                 //                    print(json[0]["name"])
             } else if let error = error {
